dc48dc7f956fca442871d58695b2a9d13a7174f8
Tristan Galloway <tristan.a.galloway@gmail.com>
2024-12-05 14:25:58 -0700

    Added base API request

---
+++ data/api-info.js
@@ -0,0 +1,17 @@
+const key = "2d44f7929c694e258273a05231317948";
+
+const weatherURL = `https://api.rawg.io/api/games?key=${key}`
+
+async function apiFetch(url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
